3e65aa97d10688a5e58e7decbd69942554a91a07
Alrawanhope <rawan@sataiva.com>
2022-05-16 15:54:54 +0530

    Controls changed from OrbitControls to  PresentationControls

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,12 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Html, useProgress, OrbitControls, useGLTF } from "@react-three/drei";
+import {
+  Html,
+  useProgress,
+  // OrbitControls,
+  useGLTF,
+  PresentationControls,
+} from "@react-three/drei";
 import {
   homeSession,
   homeHeader,
@@ -40,11 +46,23 @@ const Home = ({ sanityHome }) => {
         <Canvas>
           <Suspense fallback={<Loader />}>
             <ambientLight />
+            <PresentationControls
+              global={true} // Spin globally or by dragging the model
+              cursor={true} // Whether to toggle cursor style on drag
+              snap={false} // Snap-back to center (can also be a spring config)
+              speed={2.5} // Speed factor
+              zoom={1} // Zoom factor when half the polar-max is reached
+              rotation={[0, 0, 0]} // Default rotation
+              polar={[Math.PI / 7.5,Math.PI / 7]} // Vertical limits
+              azimuth={[-Infinity, Infinity]} // Horizontal limits
+              config={{ mass: 1, tension: 170, friction: 26 }} // Spring config
+            >
             <mesh position={[0, -3.5, 0]} scale={[3.5, 3.5, 3.5]}>
               <Model url={sanityHome.modal.asset.url} />
             </mesh>
+            </PresentationControls>
           </Suspense>
-          <OrbitControls
+          {/* <OrbitControls
             enableZoom={false}
             autoRotate={true}
             rotateSpeed={1.1}
@@ -53,7 +71,7 @@ const Home = ({ sanityHome }) => {
             dampingFactor={0.05}
             minPolarAngle={Math.PI / 2.5}
             maxPolarAngle={Math.PI / 2.5}
-          />
+          /> */}
         </Canvas>
       </div>
     </section>
